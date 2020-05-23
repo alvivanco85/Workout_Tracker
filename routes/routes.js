f81d7d210fb3838b747703722d815bb4b66b2d69
Linky Linkerson <aldovibanco85@gmail.com>
2020-05-22 17:39:48 -0700

    still trying to fix app

--- routes/routes.js
+++ routes/routes.js
@@ -1,30 +1,60 @@
 const router = require("express").Router();
-const Fitness = require(`../models/index.js`);
+const Workout = require("../models/index.js");
 
-router.post(`/api/workouts`, (req, res) => {
-  Fitness.create({}).then((dbWorkouts) => {
-    res.json(dbWorkouts);
-  });
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 router.put("/api/workouts/:id", ({ body, params }, res) => {
-  Fitness.findByIdAndUpdate(params.id, { $push: { exercises: body } }).then(
-    (dbWorkouts) => {
-      res.json(dbWorkouts);
-    }
-  );
+  Workout.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    // "runValidators" will ensure new exercises meet our schema requirements
+    { new: true, runValidators: true }
+  )
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 router.get("/api/workouts", (req, res) => {
-  Fitness.find().then((dbWorkouts) => {
-    res.json(dbWorkouts);
-  });
+  Workout.find()
+    .then(dbWorkouts => {
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
-router.get("/api/workouts/range", ({ query }, res) => {
-  Fitness.find().then((dbWorkouts) => {
-    res.json(dbWorkouts);
-  });
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({}).limit(7)
+    .then(dbWorkouts => {
+      console.log(dbWorkouts)
+      res.json(dbWorkouts);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.delete("/api/workouts", ({ body }, res) => {
+  Workout.findByIdAndDelete(body.id)
+    .then(() => {
+      res.json(true);
+    })
+    .catch(err => {
+      res.json(err);
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,28 +1,27 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
-const routes = require("./controllers/controller.js");
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(routes);
-
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false,
+  useFindAndModify: false
 });
 
+// routes
 app.use(require("./routes/routes.js"));
-app.use(require("./routes/html-routes"));
+app.use(require("./routes/html-routes.js"));
 
 app.listen(PORT, () => {
-  console.log(`localhost:${PORT}!`);
+  console.log(`App running on port ${PORT}!`);
 });
